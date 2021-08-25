353c9e93665b2a58ded65dfa7ac6a54889392f3d
Baatarvan <baatarvan@LEAP-19s-iMac.local>
2021-08-25 11:30:00 +0800

    darkmode

--- App.js
+++ App.js
@@ -1,118 +1,40 @@
-import React from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import React, {useState} from 'react';
+import {View, Switch, StyleSheet, Text} from 'react-native';
 
-const Card = ({title, score, description}) => {
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleSwitch = () => setDarkMode(previousState => !previousState);
   return (
-    <View style={styles.row}>
-      <Image
-        style={styles.poster}
-        source={{
-          uri: 'https://upload.wikimedia.org/wikipedia/en/6/60/The_Tomorrow_War_%282021_film%29_official_theatrical_poster.jpg',
-        }}
+    <View style={darkMode ? styles.light : styles.dark}>
+      <Switch
+        trackColor={{false: '#767577', true: '#fff'}}
+        thumbColor={darkMode ? '#00ff00' : '#fff'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={darkMode}
       />
-      <View style={styles.flex2}>
-        <View style={styles.insideflex2}>
-          <Text style={{textAlignVertical: 'center', textAlign: 'center'}}>
-            {title}
-          </Text>
-          <View style={styles.Description}>
-            <Text>{score}</Text>
-            <Text>{description}</Text>
-          </View>
-        </View>
-        <View style={styles.insideflex1}>
-          <Image
-            style={styles.like}
-            source={{
-              uri: 'https://png.pngtree.com/png-vector/20190223/ourmid/pngtree-vector-like-icon-png-image_695769.jpg',
-            }}
-          />
-        </View>
-      </View>
-    </View>
-  );
-};
-
-export const Flex = () => {
-  return (
-    <View style={[styles.container]}>
-      <Text>Movies</Text>
-      <Card
-        title="The Tomorrow War"
-        score="8.3"
-        description="this is description"
-      />
-      <Card
-        title="The Tomorrow War"
-        score="8.3"
-        description="this is description"
-      />
-      <Card
-        title="The Tomorrow War"
-        score="8.3"
-        description="this is description"
-      />
-      <Card
-        title="The Tomorrow War"
-        score="8.3"
-        description="this is description"
-      />
-      <Card
-        title="The Tomorrow War"
-        score="8.3"
-        description="this is description"
-      />
-      <Card
-        title="The Tomorrow War"
-        score="8.3"
-        description="this is description"
-      />
+      {darkMode ? (
+        <Text style={{color: 'white'}}>This Dark mode</Text>
+      ) : (
+        <Text>This is Light mode</Text>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 20,
-    paddingVertical: 50,
-    backgroundColor: 'grey',
+  light: {
     flex: 1,
-  },
-  row: {
-    flexDirection: 'row',
-  },
-
-  flex: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-
-  flex2: {
-    flex: 2,
-    flexDirection: 'row',
-  },
-  insideflex2: {
-    flex: 2,
-    justifyContent: 'space-around',
-  },
-  Description: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
-  insideflex1: {
-    flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
   },
-  poster: {
-    width: 100,
-    height: 120,
-  },
-
-  like: {
-    width: 40,
-    height: 40,
+  dark: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
   },
 });
 
-export default Flex;
+export default App;
