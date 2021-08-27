824a315e30c3541a4a22a8ff67e0f00f7c5965d7
Baatarvan Tumurbaatar <baatar04@gmail.com>
2021-08-27 14:18:16 +0800

    almost done

--- App.js
+++ App.js
@@ -1,39 +1,76 @@
-import React, {useState} from 'react';
-import {View, Switch, StyleSheet, Text} from 'react-native';
+import React, {useRef, useEffect} from 'react';
+import {
+  Image,
+  View,
+  Animated,
+  Text,
+  FlatList,
+  StyleSheet,
+  ImageBackground,
+  Dimensions,
+} from 'react-native';
+const {width, height} = Dimensions.get('window');
 
-const App = () => {
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleSwitch = () => setDarkMode(previousState => !previousState);
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    url: 'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/89058/93685/Joker-2019-Final-Style-steps-Poster-buy-original-movie-posters-at-starstills__62518.1572351179.jpg?c=2?imbypass=on',
+  },
+  {
+    id: 'bdd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    url: 'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/89058/93685/Joker-2019-Final-Style-steps-Poster-buy-original-movie-posters-at-starstills__62518.1572351179.jpg?c=2?imbypass=on',
+  },
+  {
+    id: 'bdsd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    url: 'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/89058/93685/Joker-2019-Final-Style-steps-Poster-buy-original-movie-posters-at-starstills__62518.1572351179.jpg?c=2?imbypass=on',
+  },
+];
+
+const Item = ({url, animation}) => (
+  <ImageBackground style={styles.item} source={{uri: url}} blurRadius={80}>
+    <Animated.View style={[styles.image, {transform: [{scale: animation}]}]}>
+      <Image source={{uri: url}} style={styles.image} />
+    </Animated.View>
+  </ImageBackground>
+);
+export const App = () => {
+  const scaleAnimation = useRef(new Animated.Value(0.2)).current;
+  useEffect(() => {
+    Animated.timing(scaleAnimation, {
+      toValue: 1,
+      duration: 1999,
+      useNativeDriver: true,
+    }).start();
+  });
+
+  const renderItem = ({item}) => (
+    <Item url={item.url} animation={scaleAnimation} />
+  );
+
   return (
-    <View style={darkMode ? styles.light : styles.dark}>
-      <Switch
-        trackColor={{false: '#767577', true: '#fff'}}
-        thumbColor={darkMode ? '#00ff00' : '#fff'}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={darkMode}
+    <View>
+      <FlatList
+        horizontal
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
       />
-      {darkMode ? (
-        <Text style={{color: 'white'}}>This Dark mode</Text>
-      ) : (
-        <Text>This is Light mode</Text>
-      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  light: {
-    flex: 1,
-    alignItems: 'center',
+  item: {
+    display: 'flex',
     justifyContent: 'center',
-    backgroundColor: 'black',
+    alignItems: 'center',
+    width: width,
+    height: height,
   },
-  dark: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
+  image: {
+    width: width - 150,
+    height: height - 300,
+    borderRadius: 20,
   },
 });
 
