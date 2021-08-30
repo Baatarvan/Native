72dfe1fdb6aebc699ae443e31c09e18e311e79d5
Baatarvan Tumurbaatar <baatar04@gmail.com>
2021-08-30 14:49:48 +0800

    add image

--- App.js
+++ App.js
@@ -1,51 +1,81 @@
 import React from 'react';
 import {
-  SafeAreaView,
-  View,
   Image,
+  Dimensions,
+  View,
   FlatList,
   StyleSheet,
   Text,
-  StatusBar,
 } from 'react-native';
+
+const {width: ScreenWidth, height: ScreenHeight} = Dimensions.get('window');
 
 const DATA = [
   {
-    title: 'this is title',
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    image:
-      'https://content.ikon.mn/news/2021/8/30/1frajx_241041022_1127070227702385_3368701010442681018_n_x974.jpg',
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53dabb28ba',
+    hotel: 'Upper West Side',
+    apartment: 'The Belnord Apartment',
+    address: '225W 86th St, New York, NY, 10024',
+    cost: '$5,250 month',
+    background:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png',
   },
 ];
-const Item = ({title, image}) => (
-  <View style={styles.item}>
-    <Image source={{uri: image}} stlye={styles.image} />
-    <Text>{title}</Text>
-  </View>
+
+const Item = ({background, hotel, apartment, address, cost}) => (
+  <>
+    <Image
+      style={styles.background}
+      source={{
+        uri: background,
+      }}
+    />
+
+    <View>
+      <Text>{hotel}</Text>
+      <Text>{apartment}</Text>
+      <Text>{address}</Text>
+      <Text>{cost}</Text>
+    </View>
+  </>
+);
+
+const renderItem = ({item}) => (
+  <Item
+    background={item.background}
+    hotel={item.hotel}
+    apartment={item.apartment}
+    address={item.address}
+    cost={item.cost}
+  />
 );
 
 const App = () => {
-  const renderItem = ({item}) => <Item title={item.title} image={item.image} />;
-
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <FlatList
         data={DATA}
         renderItem={renderItem}
         keyExtractor={item => item.id}
       />
-    </SafeAreaView>
+    </View>
   );
 };
+
 export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
   },
-  image: {
-    width: 200,
-    height: 100,
+  background: {
+    width: ScreenWidth,
+    height: ScreenHeight / 4,
+  },
+  item: {
+    alignItems: 'center',
+    backgroundColor: 'green',
+    borderRadius: 10,
+    marginBottom: 3,
   },
 });
