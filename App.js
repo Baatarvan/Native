4a6341b3ce86ae83342b38c7ce3367b6100bae30
Baatarvan Tumurbaatar <baatar04@gmail.com>
2021-08-30 13:22:37 +0800

    pre

--- App.js
+++ App.js
@@ -1,76 +1,51 @@
-import React, {useRef, useEffect} from 'react';
+import React from 'react';
 import {
-  Image,
+  SafeAreaView,
   View,
-  Animated,
+  Image,
   FlatList,
   StyleSheet,
-  ImageBackground,
-  Dimensions,
+  Text,
+  StatusBar,
 } from 'react-native';
-const {width, height} = Dimensions.get('window');
 
 const DATA = [
   {
+    title: 'this is title',
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    url: 'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/89058/93685/Joker-2019-Final-Style-steps-Poster-buy-original-movie-posters-at-starstills__62518.1572351179.jpg?c=2?imbypass=on',
-  },
-  {
-    id: 'bdd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    url: 'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/89058/93685/Joker-2019-Final-Style-steps-Poster-buy-original-movie-posters-at-starstills__62518.1572351179.jpg?c=2?imbypass=on',
-  },
-  {
-    id: 'bdsd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    url: 'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/89058/93685/Joker-2019-Final-Style-steps-Poster-buy-original-movie-posters-at-starstills__62518.1572351179.jpg?c=2?imbypass=on',
+    image:
+      'https://content.ikon.mn/news/2021/8/30/1frajx_241041022_1127070227702385_3368701010442681018_n_x974.jpg',
   },
 ];
-
-const Item = ({url, animation}) => (
-  <ImageBackground style={styles.item} source={{uri: url}} blurRadius={80}>
-    <Animated.View style={[styles.image, {transform: [{scale: animation}]}]}>
-      <Image source={{uri: url}} style={styles.image} />
-    </Animated.View>
-  </ImageBackground>
+const Item = ({title, image}) => (
+  <View style={styles.item}>
+    <Image source={{uri: image}} stlye={styles.image} />
+    <Text>{title}</Text>
+  </View>
 );
-export const App = () => {
-  const scaleAnimation = useRef(new Animated.Value(0.2)).current;
-  useEffect(() => {
-    Animated.timing(scaleAnimation, {
-      toValue: 1,
-      duration: 1999,
-      useNativeDriver: true,
-    }).start();
-  });
 
-  const renderItem = ({item}) => (
-    <Item url={item.url} animation={scaleAnimation} />
-  );
+const App = () => {
+  const renderItem = ({item}) => <Item title={item.title} image={item.image} />;
 
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
       <FlatList
-        horizontal
         data={DATA}
         renderItem={renderItem}
         keyExtractor={item => item.id}
       />
-    </View>
+    </SafeAreaView>
   );
 };
+export default App;
 
 const styles = StyleSheet.create({
-  item: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: width,
-    height: height,
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
   },
   image: {
-    width: width - 150,
-    height: height - 300,
-    borderRadius: 20,
+    width: 200,
+    height: 100,
   },
 });
-
-export default App;
